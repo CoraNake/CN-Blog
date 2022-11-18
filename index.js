b969189fcfb6b86e9a957e1d749a117dff60f607
CoraNake <118303388+CoraNake@users.noreply.github.com>
2022-11-18 13:46:51 -1000

    2 files modified

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
-const card = `
-  <div class="col-4 col-md-4 col-sm-12">
-    <div class="card mb-5 shadow-sm">
-      <img src="https://picsum.photos/1000/600" class="img-fluid" />
+const card = (title, description, image) => ` <div class="col-lg-4 col-sm-12"> <div class="card mb-5 shadow-sm"><img src="${image}" class="img-fluid" /><div class="card-body"> <div class="card-title"/a> <h2>${title}</h2> </div> <div class="card-text"> <p>${description}</p> </div> <a href="#" class="btn btn-outline-info rounded-0 float-end">Read More</a> </div> </div> </div> `;
 
-      <div class="card-body">
-        <div class="card-title">
-          <h2>This is the post title</h2>
-        </div>
-        <div class="card-text">
-          <p>
-            En effet, mon profil correspond à la description recherchée sur
-            l’offre d’emploi (préciser où l’annonce a été vue). (Si le candidat
-            possède peu d’expérience professionnelle) Ma formation en (préciser la
-            formation) m'a permis d'acquérir de nombreuses compétences parmi
-            celles que vous recherchez. Je possède tous les atouts qui me
-            permettront de réussir dans le rôle que vous voudrez bien me confier.
-            Motivation, rigueur et écoute sont les maîtres mots de mon
-            comportement professionnel...
-          </p>
-        </div>
-        <a href="#" class="btn btn-outline-info rounded-0 float-end">Read More</a>
-      </div>
-    </div>
-  </div>
-`;
+
 
 (async () => {
   const apiData = await fetch(
     'https://jsonplaceholder.typicode.com/posts'
   ).then((response) => response.json());
-  console.log(apiData);
-  const dataDiv = document.getElementById('data');
-  dataDiv.innerHTML = card;
+
+  const myData = [
+    {
+      title: 'Mardi 15 Novembre 2022',     
+      description: 'Rattrapage du retard: J\'ai rattraper mon retard de la veille en apprenant comment se décompose un site web, quelles utilités ont les trois langages informatiques HTML;CSS et Javascript.<br> Configuration de GitHub,Discord et Codesandbox.La matiné a consiter à un rattrapage intensif des bases de l\'écriture HTML et CSS avec le projet de faire un CV sur Codesanbox.<br> L\'après-midi a consisté à de la pratique sur Javascript avec le projet de faire un cube qui changait de couleur.',      
+      image: "https://picsum.photos/1000/600",  
+    },
+    {
+      title: 'Mercredi 16 Novembre 2022',
+      description: 'Matin: Table ronde autour du partage de nouvelles découvertes dans le monde du Code. Transition de Codesandbox à Stackblitz.<br> Panne de courant donc exercice de création du jeu \'Morpion\' dans l\'attente d\'internet.<br> Retour du courant: Apprentissage de Bootstrap. Après-midi: Création du projet Portfolio.',
+      image: "https://picsum.photos/1000/600",  
+    },
+    {
+      title: 'Jeudi 17 Novembre 2022',
+      description: 'Télétravail : Suivi du projet sur le Portfolio grâce à la vidéo tutoriel partagé dans le Discord.',
+      image: "https://picsum.photos/1000/600",
+      },
+      {
+      title: 'Vendredi 18 Novembre 2022',
+      description: 'Rattrapage du retard: Finition du projet Portfolio grâce au partage du code de Maru.<br> Puis création du Blog et mise en forme du Blog avec Javascript et HTML. Encore en configuration.',
+      image: "https://picsum.photos/1000/600",  
+    },
+      {
+      title: 'Lundi 21 Novembre 2022', 
+      description: 'A suivre.',
+      image: "https://picsum.photos/1000/600",  
+    },
+    {
+      title: 'Mardi 22 Novembre 2022',
+      description: 'A suivre.',
+      image: "https://picsum.photos/1000/600",     
+      }                           
+  ]
+
+  myData.forEach((post) => { 
+    document.getElementById('card-list').innerHTML += card(post.title, post.description, post.image)
+  });
 })();
